d660044b3a99615a150f14dbc05c2b7985228511
SevilG84 <sevilg84@gmail.com>
2023-12-18 20:45:23 -0500

    UserAPI2

--- tests/test_users.py
+++ tests/test_users.py
@@ -17,6 +17,7 @@ class TestUsers(TestBase):
         assert data['data']['id'] == 10
         assert 'data' in data.keys()
         assert 'support' in data.keys()
+        assert 'text' in data['support'].keys()
 
 
     # data driven test case
